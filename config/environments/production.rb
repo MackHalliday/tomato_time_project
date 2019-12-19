93a4284f82631767e5a818898b5f4c58bed578e5
MackHalliday <mackenzie.halliday@gmail.com>
2019-12-19 10:14:12 -0700

    Add cloud front env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.action_controller.asset_host = "d3uz0kz7lvt8k0.cloudfront.net"
   # Code is not reloaded between requests.
   config.cache_classes = true
 
